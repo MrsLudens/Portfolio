02da4352a511f8bf5ca106c9da3b0f4e1ebe7d13
Gabriela Silveira <46719962+MrsLudens@users.noreply.github.com>
2022-07-04 16:30:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -11,3 +11,9 @@ var btn = document.querySelector("#back-to-top");
 btn.addEventListener("click", function() {
     window.scrollTo(0, 0);
 });
+
+const chk = document.getElementById('chk')
+
+chk.addEventListener('change', () => {
+  document.body.classList.toggle('dark')
+})
